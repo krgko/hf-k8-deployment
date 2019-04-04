ed86e581b9e9e3d0a21dda9d20d6f673b5903fda
Kriengsak Ko <kriengsak.ko@kbtg.tech>
2019-04-04 23:28:58 +0700

    modify scripts, deployment configs and remove did not used configs

--- scripts/start.py
+++ scripts/start.py
@@ -1,7 +1,18 @@
 import os
+import sys
+import getopt
 
 CURRENTDIR = os.path.dirname(__file__)
 DEPLOYMDIR = os.path.join(CURRENTDIR, "../deployments")
+
+command_helper = """Usage:
+  start.py -t <pv|ca|orderer|peer>
+
+Options:
+  -h, --help            Get help options.
+  -t, --type            Specify type for deploy kubernetes service.
+                        (types are pv, ca, orderer, peer)
+"""
 
 
 def start(type):
@@ -16,5 +27,28 @@ def run(configure_name):
     os.system("kubectl apply -f " + os.path.join(DEPLOYMDIR, configure_name))
 
 
-# currently there are persistent_volume, deployment
-start("persistent_volume")
+def main(argv):
+    try:
+        opts, _ = getopt.getopt(sys.argv[1:], "ht:", ["help", "type="])
+    except getopt.GetoptError:
+        print(command_helper)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(command_helper)
+            sys.exit(0)
+        elif opt in ("-t", "--type"):
+            switcher = {
+                "pv": "persistent_volume",
+                "ca": "deployment_ca",
+                "orderer": "deployment_orderer",
+                "peer": "deployment_peer",
+            }
+            try:
+                start(switcher[arg])
+            except:
+                print(command_helper)
+                sys.exit(1)
+
+
+main(sys.argv[1:])
--- scripts/stop.py
+++ scripts/stop.py
@@ -1,11 +1,23 @@
 import os
+import sys
+import getopt
 
 CURRENTDIR = os.path.dirname(__file__)
 DEPLOYMDIR = os.path.join(CURRENTDIR, "../deployments")
 
+command_helper = """Usage:
+  stop.py -t <pv|ca|orderer|peer|all>
+
+Options:
+  -h, --help            Get help options.
+  -t, --type            Specify type for deploy kubernetes service.
+                        (types are pv, ca, orderer, peer, all)
+"""
+
 
 def stop(type):
     all_files = os.listdir(DEPLOYMDIR)
+
     # filter only matched keyword
     selected_configs = [f for f in all_files if type in f]
     for selected_config in selected_configs:
@@ -16,5 +28,29 @@ def run(configure_name):
     os.system("kubectl delete -f " + os.path.join(DEPLOYMDIR, configure_name))
 
 
-# currently there are persistent_volume, deployment
-stop("persistent_volume")
+def main(argv):
+    try:
+        opts, _ = getopt.getopt(sys.argv[1:], "ht:", ["help", "type="])
+    except getopt.GetoptError:
+        print(command_helper)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(command_helper)
+            sys.exit(0)
+        elif opt in ("-t", "--type"):
+            switcher = {
+                "pv": "persistent_volume",
+                "ca": "deployment_ca",
+                "orderer": "deployment_orderer",
+                "peer": "deployment_peer",
+                "all": ".yaml"
+            }
+            try:
+                stop(switcher[arg])
+            except:
+                print(command_helper)
+                sys.exit(1)
+
+
+main(sys.argv[1:])
