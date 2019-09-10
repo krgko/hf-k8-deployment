2a54f577a9a9f6ac681a6b5322c21b72041c60e7
origincert <kowpun123@gmail.com>
2019-09-10 15:32:20 +0700

    add mounting script and clean local storage

---
+++ scripts/clean_pv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf ../hyperledger-prerequisite/ca-server/*
+rm -rf ../hyperledger-prerequisite/ledgers/*
---
+++ scripts/mount.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Mount minikube ..."
+minikube mount ../hyperledger-prerequisite:/mnt/sda1/mounted
